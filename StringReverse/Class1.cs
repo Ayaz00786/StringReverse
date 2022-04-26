c2c97a47f7a99a4f712d62315bc04fd4933d4fff
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-26 16:20:34 +0530

    Add project files.

---
+++ StringReverse/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringReverse
+{
+    internal class Class1
+    {
+        public static void Array()
+        {
+            Console.WriteLine("Enter number of character want to use:");
+            int n= int.Parse(Console.ReadLine());
+
+            char [] array = new char[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter character in Position {0}:", i);
+                array[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Character Stored in Array Are: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}", array[i]);
+            }
+
+            Console.WriteLine("\n\nReverse String of Array :");
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", array[i]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ StringReverse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Array();
+        }
+    }
+}
